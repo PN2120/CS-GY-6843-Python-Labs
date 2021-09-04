cd97748afc184038bf1576e3b3af46f8821f67be
PN2120 <89992374+PN2120@users.noreply.github.com>
2021-09-04 12:29:43 -0400

    change updated
    change updated

--- solution.py
+++ solution.py
@@ -6,21 +6,21 @@ global answer
 def welcome_assignment_answers(question):
    # The student doesn't have to follow the skeleton for this assignment.
    # Another way to implement it is using "case" statements similar to C.
-   if   question == "Are encoding and encryption the same? - Yes/No":
+   if   question == "Are encoding and encryption the same? - Yes/No"
        answer = "No"
-   elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+   elif question == "Is it possible to decrypt a message without a key? - Yes/No"
        answer = "No"
-   elif question == "Is it possible to decode a message without a key? - Yes/No":
+   elif question == "Is it possible to decode a message without a key? - Yes/No"
        answer = "Yes"
-   elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+   elif question == "Is a hashed message supposed to be un-hashed? - Yes/No"
        answer = "No"
-   elif question == "What is the MD5 hashing value to the following message: 'Welcome to  Fall Network Computers by Mr.Portnoy' - Use MD5 hash generator and use the answer in your code":
+   elif question == "What is the MD5 hashing value to the following message: 'Welcome to  Fall Network Computers by Mr.Portnoy' - Use MD5 hash generator and use the answer in your code"
        answer = "3d67db7bb3472e545bdb75aa2fc65322"
-   elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+   elif question == "Is MD5 a secured hashing algorithm? - Yes/No"
        answer = "No"
-   elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+   elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
        answer = 5
-   elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+   elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
        answer = 4
    return (answer)
 
