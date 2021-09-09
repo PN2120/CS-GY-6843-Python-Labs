037e0b4b002cbd1b82f28bf64791c3d1f4042a03
PN2120 <89992374+PN2120@users.noreply.github.com>
2021-09-08 20:52:03 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 ### welcome_assignment_answers
 ### Input - All eight questions given in the assignment.
 ### Output - The right answer for the specific question.
-### PN2120
+
 
 def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
