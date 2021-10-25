30d5ff00e708d442014a872d256c4cb9d4f77c74
PN2120 <89992374+PN2120@users.noreply.github.com>
2021-10-25 19:25:42 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -23,24 +23,23 @@ def checksum(str):
     countTo = (len(str) / 2) * 2 
     count = 0
 
-while count < countTo:
-    thisVal = ord(str[count+1]) * 256 + ord(str[count])
+    while count < countTo:
+        thisVal = ord(str[count+1]) * 256 + ord(str[count])
  
-    csum = csum + thisVal 
-    csum = csum & 0xffffffff
-    count = count + 2
+        csum = csum + thisVal 
+        csum = csum & 0xffffffff
+        count = count + 2
 
-if countTo < len(str):
-    csum = csum + ord(str[len(str) - 1]) 
-    csum = csum & 0xffffffff
+        if countTo < len(str):
+            csum = csum + ord(str[len(str) - 1]) 
+            csum = csum & 0xffffffff
 
-csum = (csum >> 16) + (csum & 0xffff) 
-csum = csum + (csum >> 16)
-answer = ~csum
-answer = answer & 0xffff
-answer = answer >> 8 | (answer << 8 & 0xff00) 
-
-return answer
+    csum = (csum >> 16) + (csum & 0xffff) 
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00) 
+    return answer
 
 def build_packet():
 # In the sendOnePing() method of the ICMP Ping exercise ,firstly the header of our
@@ -65,31 +64,32 @@ def build_packet():
 
 def get_route(hostname): 
     timeLeft = TIMEOUT
-    for ttl in xrange(1,MAX_HOPS): 
-        for tries in xrange(TRIES):
+    for ttl in range(1,MAX_HOPS): 
+        for tries in range(TRIES):
             destAddr = socket.gethostbyname(hostname)
             icmp = socket.getprotobyname("icmp")
             mySocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp) 
             mySocket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, struct.pack('I', ttl)) 
             mySocket.settimeout(TIMEOUT)
             try:
-                d = build_packet() mySocket.sendto(d, (hostname, 0)) 
+                d = build_packet() 
+                mySocket.sendto(d, (hostname, 0)) 
                 t = time.time()
                 startedSelect = time.time()
                 whatReady = select.select([mySocket], [], [], timeLeft) 
                 howLongInSelect = (time.time() - startedSelect)
                 if whatReady[0] == []: # Timeout 
-                    print "* * * Request timed out."
+                    print("* * * Request timed out.")
 
                 recvPacket, addr = mySocket.recvfrom(1024) 
-                print addr
+                print(addr)
                 timeReceived = time.time()
                 timeLeft = timeLeft - howLongInSelect 
                 if timeLeft <= 0:
-                     print "* *   * Request timed out."
-                except socket.timeout:
+                    print("* *   * Request timed out.")
+            except socket.timeout:
                     continue
-                else:
+            else:
 # Fetch the icmp type from the IP packet 
                     icmpHeader = recvPacket[20:28]
                     request_type, code, checksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
@@ -97,20 +97,20 @@ def get_route(hostname):
                     if request_type == 11:
                         bytes = struct.calcsize("d")
                         timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                        print " %d rtt=%.0f ms %s" % (ttl,(timeReceived -t)*1000, addr[0]) 
+                        print(" %d rtt=%.0f ms %s" % (ttl,(timeReceived -t)*1000, addr[0]))
                     elif request_type == 3:
                         bytes = struct.calcsize("d")
                         timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                        print " %d rtt=%.0f ms %s" % (ttl,(timeReceived -t)*1000, addr[0]) 
+                        print(" %d rtt=%.0f ms %s" % (ttl,(timeReceived -t)*1000, addr[0]))
                     elif request_type == 0:
                         bytes = struct.calcsize("d")
                         timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                        print " %d rtt=%.0f ms %s" % (ttl,(timeReceived -timeSent)*1000, addr[0]) 
+                        print( " %d rtt=%.0f ms %s" % (ttl,(timeReceived -timeSent)*1000, addr[0]) )
                         return
                     else:
-                        print "error" 
+                        print( "error" )
                         break
-                        finally:
-                            mySocket.close()
+            finally:
+                mySocket.close()
 
 get_route("www.google.com")
